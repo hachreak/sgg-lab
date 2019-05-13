7a81b62b88fcaeab4aa94e76f0e94845efcc030e
Leonardo Rossi <leonardo.rossi@unipr.it>
2019-05-13 16:12:20 +0200

    datasets: open images dataset
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>

---
+++ sgg_lab/datasets/open_images.py
@@ -0,0 +1,115 @@
+
+"""Open Images Dataset."""
+
+import os
+
+from copy import deepcopy
+from collections import defaultdict
+
+from . import load_csv
+
+
+#  OBJS_LABELS = 'challenge-2019-classes-description-500.csv'
+#  OBJS_LABELS_SEGM = 'challenge-2019-classes-description-segmentable.csv'
+
+VRD_OBJS_LABELS = 'challenge-2019-classes-vrd.csv'
+VRD_TRAIN = 'challenge-2019-train-vrd.csv'
+
+OD_BBOX_TRAIN = 'challenge-2019-train-detection-bbox.csv'
+
+
+def get_relationship_triplets(filename):
+    """Get visual relationships."""
+    vrds = defaultdict(list)
+    for r in load_csv(filename):
+        vrds[r[0]].append({
+            "image_id": r[0],
+            "relationship": r[11],
+            "objects": [
+                {
+                    "label": r[1],
+                    "bbox": [r[3], r[5], r[4], r[6]]
+                },
+                {
+                    "label": r[2],
+                    "bbox": [r[7], r[9], r[8], r[10]]
+                },
+            ]
+        })
+    return vrds
+
+
+def get_mask_image(images_path):
+    """Convert in a image file."""
+    filenames = os.listdir(images_path)
+
+    def f(image_id, label_id):
+        base_name = '{0}_{1}'.format(image_id, label_id.replace('/', ''))
+        return filter(lambda x: x.startswith(base_name), filenames)[0]
+    return f
+
+
+def stream_vrds(vrds, get_image_path):
+    for k, v in vrds.items():
+        v = deepcopy(v)
+        pairs = []
+        for pair in v:
+            founds = 0
+            for o in pair['objects']:
+                try:
+                    o['path'] = get_image_path(k, o['label'])
+                    founds += 1
+                except IndexError:
+                    pass
+            if len(pair['objects']) == founds:
+                pairs.append(pair)
+        if len(pairs) > 0:
+            yield k, pairs
+
+
+#  def get_vrd(filename):
+#      """Get visual relationships."""
+#      vrds = defaultdict(list)
+#      for r in load_csv(filename):
+#          vrds[r[0]].append({
+#              "relationship": r[2],
+#              "objects": [
+#                  {
+#                      "label": r[0],
+#                  },
+#                  {
+#                      "label": r[1],
+#                  },
+#              ]
+#          })
+#      return vrds
+
+
+#  def get_seg_bbox(filename):
+#      """Get segmentation label and bbox."""
+#      vrds = defaultdict(list)
+#      for r in load_csv(filename):
+#          vrds[r[0]].append({
+#              "relationship": r[11],
+#              "objects": [
+#                  {
+#                      "label": r[1],
+#                      "bbox": [r[3], r[5], r[4], r[6]]
+#                  },
+#                  {
+#                      "label": r[2],
+#                      "bbox": [r[7], r[9], r[8], r[10]]
+#                  },
+#              ]
+#          })
+#      return vrds
+
+
+def translate(filename):
+    """Translate label."""
+    labels = {k: v for [k, v] in load_csv(filename, skip_headers=False)}
+
+    def f(label_id):
+        return labels[label_id]
+
+    return f
