34e8ec108a5148e8503cefca728e9659f02d2c49
Leonardo Rossi <leonardo.rossi@unipr.it>
2019-06-18 11:31:37 +0200

    callbacks: configurable save best
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>

--- sgg_lab/callbacks.py
+++ sgg_lab/callbacks.py
@@ -11,20 +11,30 @@ def avg_logs(logs, filter_cond):
     return np.average([v for k, v in logs.items() if filter_cond(k)])
 
 
+def filter_val_accs(l):
+    """Filter condition: only validation accuracy."""
+    return l.startswith('val_') and l.endswith('acc')
+
+
+def filter_val_f1score(l):
+    """Filter condition: only validation f1score."""
+    return l.startswith('val_') and l.endswith('f1_score')
+
+
 class ModelSaveBestAvgAcc(Callback):
 
-    def __init__(self, filepath, verbose=False):
+    def __init__(self, filepath, verbose=False, cond=None):
         """Init."""
         self._acc = 0
         self._filepath = filepath
         self._verbose = verbose
-        self._cond = lambda l: l.startswith('val_') and l.endswith('acc')
+        self._cond = cond or filter_val_accs
 
     def on_epoch_end(self, epoch, logs=None):
         """Save best model checking every epoch."""
         acc = avg_logs(logs, self._cond) if logs else 0
         if acc > self._acc:
-            filepath = self._filepath.format(epoch=epoch + 1, acc=acc, **logs)
+            filepath = self._filepath.format(epoch=epoch+1, avgacc=acc, **logs)
             if self._verbose:
                 print('\nEpoch %05d: saving model to %s' % (
                     epoch + 1, filepath))
